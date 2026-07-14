71408d0b8b1899d2ed78e640da4978b83e8b6f4b
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-07-14 13:37:13 +0200

    feat/SistemaGestion: El objetivo es agregar un EJB Local al proyecto SGA donde se podra ver un listado de personas, donde la interfaz local y remota expondran los mismos metodos

---
+++ Curso-Java-Udemy-Cero-a-Experto/Java-Enterprise-Edition-JEE/Sga-Jee-Web/src/main/java/avilawebservice/servicio/PersonaServiceImpl.java
@@ -0,0 +1,41 @@
+package avilawebservice.servicio;
+
+import avilawebservice.domain.Persona;
+import jakarta.ejb.Stateless;
+import java.util.ArrayList;
+import java.util.List;
+
+@Stateless
+public class PersonaServiceImpl implements PersonaServiceRemote, IPersonaService{
+
+    @Override
+    public List<Persona> listarPersonas() {
+        List<Persona> personas = new ArrayList<>();
+        personas.add(new Persona(1, "Juan", "Perez", "jperez@mail.com", "3434343434"));
+        personas.add(new Persona(2, "Martha", "Suarex", "masua@mail.com", "23232323"));
+        return personas;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return null;
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+    }
+
+}
