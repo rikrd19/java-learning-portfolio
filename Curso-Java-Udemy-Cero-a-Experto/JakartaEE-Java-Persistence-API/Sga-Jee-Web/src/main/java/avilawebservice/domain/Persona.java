3d0ae7a8be33eccaa157e6f6f110d5d04c48ba36
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-07-16 09:49:36 +0200

    feat/Conexion Mysql: realizacion de conexion desde la BD desde Netbeans colocando la libreria de MySQL y copiarla donde se realiza el  proyecto

--- Curso-Java-Udemy-Cero-a-Experto/JakartaEE-Java-Persistence-API/Sga-Jee-Web/src/main/java/avilawebservice/domain/Persona.java
+++ Curso-Java-Udemy-Cero-a-Experto/JakartaEE-Java-Persistence-API/Sga-Jee-Web/src/main/java/avilawebservice/domain/Persona.java
@@ -5,17 +5,22 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.NamedQueries;
+import jakarta.persistence.NamedQuery;
 import jakarta.persistence.Table;
 import java.io.Serializable;
 
 @Entity
+@NamedQueries({
+    @NamedQuery(name="Persona.findAll", query="SELECT p FROM Persona p ORDER BY p.idPersona")
+})
 @Table(name = "persona")  // se tiene que colocar en nombre de la tabla, sino toma y crea una con el nombre de la clase
 public class Persona implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_persona")
     private int idPersona;
 
@@ -26,7 +31,7 @@ public class Persona implements Serializable {
 
     public Persona() {
     }
-    
+
     public Persona(String nombre, String apellido, String email, String telefono) {
         this.nombre = nombre;
         this.apellido = apellido;
@@ -79,7 +84,4 @@ public class Persona implements Serializable {
         return "Persona{" + "idPersona=" + idPersona + ", nombre=" + nombre + ", apellido=" + apellido + ", email=" + email + ", telefono=" + telefono + '}';
     }
 
-    
-    
-    
 }
