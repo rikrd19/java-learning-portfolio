9ac548cd508cdfb71fbc4893780d0e0b3cc8430b
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-07-18 18:38:59 +0200

    feat/Consulta SPQL: consulta de un objeto persona por clave nombre donde se desplegara una lista de todos las coincidencias

--- Curso-Java-Udemy-Cero-a-Experto/JakartaEE-Consultas-Con-JavaPersitenceQueryLanguage-JPQL/avilawebSga-Jee-Web-Java-Persistence-Query-Language/src/main/java/avilawebservice/jpql/PruebaJPQL.java
+++ Curso-Java-Udemy-Cero-a-Experto/JakartaEE-Consultas-Con-JavaPersitenceQueryLanguage-JPQL/avilawebSga-Jee-Web-Java-Persistence-Query-Language/src/main/java/avilawebservice/jpql/PruebaJPQL.java
@@ -11,37 +11,46 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
 public class PruebaJPQL {
-    
-     static Logger log = LogManager.getRootLogger();
-     
-     public static void main(String[] args) {
-         String jpql = null;
-         Query q = null;
-         List<Persona> personas = null;
-         Persona persona = null;
-         Iterator iter = null;
-         Object[] tupla = null;
-         List<String> nombres = null;
-         List<Usuario> usuarios = null;
-         
-         EntityManagerFactory emf = Persistence.createEntityManagerFactory("SgaPU");
-         EntityManager em = emf.createEntityManager();
-         
-         // Paso 1. Consulta de todos los objetos de tipo Persona
-         log.debug("\n1. Consulta de todas las Personas");
-         jpql = "select p from Persona p";
-         personas = em.createQuery(jpql).getResultList();   // devuelve una lista de tipo persona
-         // metodo de tipo persona encargado de iterar los tipos persona a recibir
-         mostrarPersonas(personas);
-         
-         
-         
+
+    static Logger log = LogManager.getRootLogger();
+
+    public static void main(String[] args) {
+        String jpql = null;
+        Query q = null;
+        List<Persona> personas = null;
+        Persona persona = null;
+        Iterator iter = null;
+        Object[] tupla = null;
+        List<String> nombres = null;
+        List<Usuario> usuarios = null;
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("SgaPU");
+        EntityManager em = emf.createEntityManager();
+
+        // Paso 1. Consulta de todos los objetos de tipo Persona
+        log.debug("\n1. Consulta de todas las Personas");
+        jpql = "select p from Persona p";
+        personas = em.createQuery(jpql).getResultList();   // devuelve una lista de tipo persona
+        // metodo de tipo persona encargado de iterar los tipos persona a recibir
+        // mostrarPersonas(personas); se comenta para hacer la sgte Consulta
+
+        // 2 consulta filtrandon una persona con la clave primaria Persona con id=1
+        log.debug("\n2. Consulta de la Persona con id=1");
+        jpql = "select p from Persona p where p.idPersona = 1";
+        persona = (Persona) em.createQuery(jpql).getSingleResult();
+       // log.debug(persona); se comenta para hacer la nueva sentencia
+       
+       // 3. Consulta de la persona por nombre
+       jpql = "select p from Persona p where p.nombre='Karla'";
+       personas = em.createQuery(jpql).getResultList();
+        mostrarPersonas(personas);
+       
+      
     }
 
     private static void mostrarPersonas(List<Persona> personas) {
-            for (Persona p : personas) {
+        for (Persona p : personas) {
             log.debug(p);
         }
 
