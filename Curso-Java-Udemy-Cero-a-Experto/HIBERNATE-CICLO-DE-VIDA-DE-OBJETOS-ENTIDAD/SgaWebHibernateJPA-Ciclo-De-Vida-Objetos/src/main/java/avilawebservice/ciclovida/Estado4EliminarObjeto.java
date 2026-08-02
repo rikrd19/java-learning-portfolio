ca55256aa299ce13adbe246260a2c2fffda6030b
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-08-02 17:52:09 +0200

    feat/Eliminar Objetos: se recupera un objeto existente en la BD para luego removerlo(eliminar), pero antes se sincroniza el objeto en la BD, el objeto deja de existir , revisando la BD ya no existe

--- Curso-Java-Udemy-Cero-a-Experto/HIBERNATE-CICLO-DE-VIDA-DE-OBJETOS-ENTIDAD/SgaWebHibernateJPA-Ciclo-De-Vida-Objetos/src/main/java/avilawebservice/ciclovida/Estado4EliminarObjeto.java
+++ Curso-Java-Udemy-Cero-a-Experto/HIBERNATE-CICLO-DE-VIDA-DE-OBJETOS-ENTIDAD/SgaWebHibernateJPA-Ciclo-De-Vida-Objetos/src/main/java/avilawebservice/ciclovida/Estado4EliminarObjeto.java
@@ -5,32 +5,31 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
-public class Estado3ModificarObjetoPersistente {
+
+
+public class Estado4EliminarObjeto {
     
-    public static void main(String[] args) {
+      public static void main(String[] args) {
         
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("HibernateJpaPU");
         EntityManager em = emf.createEntityManager();
-
+        
         //definimos la variable
         Contacto contacto = null;
-
-        // Recuperar un objeto de la base de datos 
-        // 1. Transitivo
-        contacto = em.find(Contacto.class, 5);
+        
+         // Recuperar un objeto de la base de datos 
+         // 1. Transitivo
+       contacto = em.find(Contacto.class, 5);
         
         em.getTransaction().begin();  // se recupera el contacto antes 
-
-        //  modificamos el objeto
-        contacto.setEmail("clara@mail.com");
-
-        // 2. persistente
-//        em.persist(contacto);
-        em.merge(contacto);
+        
+        //3. remove
+        em.remove(em.merge(contacto));  // Primero se sincroniza en la base de datos
         
         em.getTransaction().commit();  // se guarda el contacto despues
-
-        // 3. detached
-        System.out.println("Contacto: " + contacto);
+        
+       // 3. transitivo
+          System.out.println("Contacto: "+contacto);
     }
+      
 }
