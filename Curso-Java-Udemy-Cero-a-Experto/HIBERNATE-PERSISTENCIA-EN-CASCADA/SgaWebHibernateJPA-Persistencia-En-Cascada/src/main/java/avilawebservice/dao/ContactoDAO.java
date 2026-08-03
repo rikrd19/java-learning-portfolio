95d6c7949a3200856c658c94a27db5b3171d6668
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-08-03 11:08:44 +0200

    feat/PersistenciaCascada: al colocar en @ManyToOne el tipo de cascade de una entidad/clase que esta relacionada con otras esta generara los id de las entidades relacionadas para despues hacer la persistencia en la BD de la entidad relacionada

---
+++ Curso-Java-Udemy-Cero-a-Experto/HIBERNATE-PERSISTENCIA-EN-CASCADA/SgaWebHibernateJPA-Persistencia-En-Cascada/src/main/java/avilawebservice/dao/ContactoDAO.java
@@ -0,0 +1,67 @@
+package avilawebservice.dao;
+
+import avilawebservice.domain.Contacto;
+import jakarta.persistence.Query;
+import java.util.List;
+
+public class ContactoDAO extends GenericDAO {
+
+    public List<Contacto> listar() {
+        String consulta = "SELECT c FROM Contacto c";
+        em = getEntityManager();
+        Query query = em.createQuery(consulta);
+
+        return query.getResultList();
+    }
+
+    public void insertar(Contacto contacto) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(contacto);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void actualizar(Contacto contacto) {
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.merge(contacto);
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+
+    }
+
+    public void eliminar(Contacto contacto){
+        try {
+            em = getEntityManager();
+        em.getTransaction().begin();
+        em.remove(em.merge(contacto));
+        em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace(System.out);
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+    
+    public Object buscarPorId(Contacto contacto){
+        em = getEntityManager();
+       return em.find(Contacto.class, contacto.getIdContacto());
+    }
+}
