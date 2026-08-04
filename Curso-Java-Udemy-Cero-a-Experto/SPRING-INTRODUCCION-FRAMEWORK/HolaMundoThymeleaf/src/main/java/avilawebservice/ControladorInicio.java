b4b54e23811b575f0bcd7d125131954d45d70bdc
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-08-04 17:57:34 +0200

    feat/Spring: creacion de un archivo a traves IDE de netbeans, agregando los plugins, y modificando el pom.xml, probando las salidas con la annotation de @RestController,- Creacion de ControladorInicio y el index.html configurado con el tag de thymeleaf  para que a traves del navegador se muestren los diferentes mensajes, igualmente configuracion del archivo application.properties - Se cambio a @Controller para poder hacer uso de los controladores de thymeleafcomo @Value

---
+++ Curso-Java-Udemy-Cero-a-Experto/SPRING-INTRODUCCION-FRAMEWORK/HolaMundoThymeleaf/src/main/java/avilawebservice/ControladorInicio.java
@@ -0,0 +1,26 @@
+package avilawebservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+
+    @Value("${index.saludo}")
+    private String saludo;
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        //log.info("Ejecutando el controlador rest");
+        String mensaje ="Mensaje con Thymeleaf.....";
+        model.addAttribute("mensaje", mensaje);
+        model.addAttribute("saludo", saludo);
+        return "index";
+    }
+
+}
