82ce65d46010633e2e36b91606cbbdb0459622c8
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-08-04 19:51:10 +0200

    feat/Spring: se crea una clase Persona a la cual se le agregan objetos y se muestran el navegadro a traves de index que es quien muestra en el navegador, se crea una lista y se con los tags de thymelean y Expression Language se muestran los encabezados y los objetos por el navegador

--- Curso-Java-Udemy-Cero-a-Experto/SPRING-INTRODUCCION-FRAMEWORK/HolaMundoThymeleaf/src/main/java/avilawebservice/ControladorInicio.java
+++ Curso-Java-Udemy-Cero-a-Experto/SPRING-INTRODUCCION-FRAMEWORK/HolaMundoThymeleaf/src/main/java/avilawebservice/ControladorInicio.java
@@ -1,11 +1,13 @@
 package avilawebservice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @Controller
 @Slf4j
@@ -13,13 +15,33 @@ public class ControladorInicio {
 
     @Value("${index.saludo}")
     private String saludo;
-    
+
     @GetMapping("/")
     public String inicio(Model model) {
-        //log.info("Ejecutando el controlador rest");
-        String mensaje ="Mensaje con Thymeleaf.....";
+        String mensaje = "Mensaje con Thymeleaf al navegador";
+
+        Persona persona = new Persona();
+        persona.setNombre("Juan");
+        persona.setApellido("Perez");
+        persona.setEmail("jpere@mail.com");
+        persona.setTelefono("3434334");
+
+        Persona persona2 = new Persona();
+        persona2.setNombre("Karla");
+        persona2.setApellido("Gomez");
+        persona2.setEmail("kgome@mail.com");
+        persona2.setTelefono("343434343");
+
+//       List<Persona> personas = new ArrayList();
+//       personas.add(persona);
+//       personas.add(persona);
+        //otra forma
+        var personas = Arrays.asList(persona, persona2);
+
         model.addAttribute("mensaje", mensaje);
         model.addAttribute("saludo", saludo);
+        model.addAttribute("persona", persona);
+        model.addAttribute("personas", personas);
         return "index";
     }
 
