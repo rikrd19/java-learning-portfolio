29e6655cb92fb609dbf3bcb27d112be62a5a0131
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-08-05 13:57:38 +0200

    feat/CRUD: cracion de la vista crear y modificar persona, creacion del archivo modificar de la capa de negocio, con su respectivo formulario y su persistencia en la BD, actualizacion en la BD para que los ids tengan una secuencia correlativa

---
+++ Curso-Java-Udemy-Cero-a-Experto/SPRING-MANEJO-TRANSACCIONAL/ControlClientesCrudCompletoMysql/src/main/java/avilawebservice/web/ControladorInicio.java
@@ -0,0 +1,41 @@
+package avilawebservice.web;
+
+import avilawebservice.Persona;
+import avilawebservice.service.PersonaService;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+
+    @Autowired
+    private PersonaService personaService;
+    
+    @GetMapping("/")
+    public String inicio(Model model) {
+        
+       List<Persona>  personas =  personaService.listarPersonas();
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+
+    @GetMapping("/agregar")
+    public String agregar(Persona persona){
+       return "modificar";
+    }
+    
+    @PostMapping("/guardar")
+    public String guardar(Persona persona){
+        personaService.guardar(persona);
+        
+        return "redirect:/";
+    }
+            
+    
+}
