e5fbe964a2e40cd068f379d6e1ec95ca53cdfda9
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-08-05 11:19:26 +0200

    feat/Data: creacion de la clase interface de la capa BD que extends del CrudRepository, que proporciona operaciones CRUD basicas para las entidades(trae los metodos heredados), evitando escribir codigo repetitivo y escribir manualmente, recuperandose en el navegador a traves del index.html (vista)

--- Curso-Java-Udemy-Cero-a-Experto/SPRING-DATA/HolaSpring-DATA/src/main/java/avilawebservice/web/ControladorInicio.java
+++ Curso-Java-Udemy-Cero-a-Experto/SPRING-DATA/HolaSpring-DATA/src/main/java/avilawebservice/web/ControladorInicio.java
@@ -1,6 +1,10 @@
 package avilawebservice.web;
 
+import avilawebservice.Persona;
+import avilawebservice.dao.IPersonaDao;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,11 +13,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Slf4j
 public class ControladorInicio {
 
+    @Autowired
+    private IPersonaDao personaDao;
+    
     @GetMapping("/")
     public String inicio(Model model) {
         
-//        log.info("Ejecutando el controlador Spring MVC");
-//        model.addAttribute("personas", personas);
+        var personas = personaDao.findAll();
+        model.addAttribute("personas", personas);
         return "index";
     }
 
