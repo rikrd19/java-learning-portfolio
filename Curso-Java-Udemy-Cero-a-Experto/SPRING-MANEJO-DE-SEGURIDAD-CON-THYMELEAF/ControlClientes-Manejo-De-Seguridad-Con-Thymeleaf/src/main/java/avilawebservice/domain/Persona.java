ccb47ed11bd789b24f98188ad32bb8bb25cc92a2
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-08-07 08:06:22 +0200

    feat/Seguridad: Thymeleaf agrega seguridad con las tags 'sec=' en para que el programa segun el Rol que cumpla el usuario una vez autenticado pueda tener autorizacion a las accciones del programa (editar, agregar, eliminaar....), asimismo si no esta autorizado el programa no le mostrara o no le mostratra las acciones a realizar

---
+++ Curso-Java-Udemy-Cero-a-Experto/SPRING-MANEJO-DE-SEGURIDAD-CON-THYMELEAF/ControlClientes-Manejo-De-Seguridad-Con-Thymeleaf/src/main/java/avilawebservice/domain/Persona.java
@@ -0,0 +1,81 @@
+package avilawebservice.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "persona")
+public class Persona implements Serializable{
+    
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPersona;
+    
+    @NotEmpty
+    private String nombre;
+    
+    @NotEmpty
+    private String apellido;
+    
+    @NotEmpty
+    @Email
+    private String email;
+    
+    private String telefono;
+
+    
+    
+    
+    public Persona() {
+    }
+
+    public Long getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(Long idPersona) {
+        this.idPersona = idPersona;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    
+}
