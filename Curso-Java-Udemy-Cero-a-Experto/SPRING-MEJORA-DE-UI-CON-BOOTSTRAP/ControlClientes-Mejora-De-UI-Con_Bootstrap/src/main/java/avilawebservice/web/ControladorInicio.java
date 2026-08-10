d83253fa4fdfc82bbd23822aa545470bbe297404
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-08-10 17:28:56 +0200

    Feat/bootstrap:  completado del Panel UI
    - Configuración del rol admin y botón eliminar con validación segun el rol.
     - Mostrar el saldo total de clientes, total de clientes en la BD, actualizandose dinamicamente el saldo total y la cantidad de personas que integran la lista si se elimina un cliente
    - Creacion del formulario de agregar con las validaciones en los campos
    - Creacion completo de los campos de editar , terminando la funcionalidad completa  del panel .”

--- Curso-Java-Udemy-Cero-a-Experto/SPRING-MEJORA-DE-UI-CON-BOOTSTRAP/ControlClientes-Mejora-De-UI-Con_Bootstrap/src/main/java/avilawebservice/web/ControladorInicio.java
+++ Curso-Java-Udemy-Cero-a-Experto/SPRING-MEJORA-DE-UI-CON-BOOTSTRAP/ControlClientes-Mejora-De-UI-Con_Bootstrap/src/main/java/avilawebservice/web/ControladorInicio.java
@@ -21,43 +21,52 @@ public class ControladorInicio {
 
     @Autowired
     private PersonaService personaService;
-    
+
     @GetMapping("/")
     public String inicio(Model model, @AuthenticationPrincipal User user) {
-         List<Persona>  personas =  personaService.listarPersonas();
+        List<Persona> personas = personaService.listarPersonas();
         log.info("Ejecutando el controlador Spring MVC");
-        log.info("usuario que hizo login:"+ user);
-      
+        log.info("usuario que hizo login:" + user);
+
         model.addAttribute("personas", personas);
+
+        var saldoTotal = 0D;
+        for (Persona p : personas) {
+        // saldoTotal += (p.getSaldo() != null) ? p.getSaldo() : 0D;  si en la BD el saldo es null
+            saldoTotal += p.getSaldo();
+        }
+        model.addAttribute("saldoTotal", saldoTotal);
+        model.addAttribute("totalCliente", personas.size());
+
         return "index";
     }
 
     @GetMapping("/agregar")
-    public String agregar(Persona persona){
-       return "modificar";
+    public String agregar(Persona persona) {
+        return "modificar";
     }
-    
+
     @PostMapping("/guardar")
-    public String guardar(@Valid Persona persona, Errors errores){
+    public String guardar(@Valid Persona persona, Errors errores) {
         if (errores.hasErrors()) {
             return "modificar";
         }
         personaService.guardar(persona);
         return "redirect:/";
     }
-           
+
     @GetMapping("/editar/{idPersona}")
-    public String editar(Persona persona, Model model){
+    public String editar(Persona persona, Model model) {
         persona = personaService.encontrarPersona(persona);
         model.addAttribute("persona", persona);
         return "modificar";
     }
-    
+
     @GetMapping("/eliminar")
-    public String eliminar(Persona persona){
+    public String eliminar(Persona persona) {
         personaService.eliminar(persona);
-        
+
         return "redirect:/";
     }
-    
+
 }
